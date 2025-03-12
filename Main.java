f8667c9bfb6a59517835a7e03054046b64fc46d5
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-03-12 14:20:18 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        byte age = 20;
+        int phone = 856523234;
+        long phone2 = 85652323445353L;
+        float pi = 3.14F;
+        String name = "Virender";
+        System.out.println(name);
+    }
+}

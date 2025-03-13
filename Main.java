664a21c587a7dc071b891aa8539bd9ca825481da
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-03-13 13:13:25 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,22 @@
 public class Main {
+
     public static void main(String[] args) {
-        byte age = 20;
+        /*byte age = 20;
         int phone = 856523234;
         long phone2 = 85652323445353L;
         float pi = 3.14F;
-        String name = "Virender";
-        System.out.println(name);
+        System.out.println(name);*/
+
+        //Non premitive types
+        String fname = "Virender";
+        String lname = new String("Singh");
+
+        String fullName = fname + " " + lname;
+
+        System.out.println(fullName);
+        System.out.println(fullName.charAt(4));
+
+        String replaceName = fullName.replace('e', 'a');
+        System.out.println(replaceName);
     }
 }
